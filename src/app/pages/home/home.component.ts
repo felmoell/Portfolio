51f71d17f84632a065a94e765dcf14ef8a76780a
Felix Moellerherm <felix.moellerherm@hs-osnabrueck.de>
2021-03-10 19:34:37 +0100

    Added Hompagesupport from GitApi

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,20 +20,19 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     AOS.init();
     document.getElementById("body")!.style.overflowX = "hidden";
-
-    this.gitHubService.getData().then(res => {
-      console.log(res);
+    this.gitHubService.getData().then(res => {     
       res.forEach((element, index) => {
         if (index <= 5) {
-          let t = new work(element.full_name, element.description, [], element.html_url, "");
+          let t = new work(element.full_name, element.description, [], element.html_url, element.homepage);
           this.gitHubService.getTopicsData("https://api.github.com/repos/" + element.full_name + "/topics").then(resLang => {
             resLang.names.forEach(element => {
               t.tags.push(element);
             });
             this.objArray.push(t);
           })
+          console.log(this.objArray);
         } else {
-          let t = new work(element.full_name, element.description, [], element.html_url, "");
+          let t = new work(element.full_name, element.description, [], element.html_url, element.homepage);
           this.gitHubService.getTopicsData("https://api.github.com/repos/" + element.full_name + "/topics").then(resLang => {
             resLang.names.forEach(element => {
               t.tags.push(element);
@@ -43,33 +42,25 @@ export class HomeComponent implements OnInit {
         }
       });
     })
-
-
-
     this.vitaArray.push(new vita("Schüler", "2003-2007", ["Grundschule", "", "", "", ""]))
     this.vitaArray.push(new vita("Schüler", "2007-2011", ["Gymnasium", "", "", "", ""]))
     this.vitaArray.push(new vita("Schüler", "2011-2013", ["Realschule", "", "", "", ""]))
     this.vitaArray.push(new vita("Schüler", "2013-2016", ["Berufliches Gymnasium", "Grundlagen Java und Software Engineering (UML)", "Grundlagen SQL und ER/EER", "", ""]))
     this.vitaArray.push(new vita("Student", "2016-2020", ["Universität", "Java und C/C++ in Verteilten Systemen und (Android) Anwendungen", "Webanwendungen/Hybride Apps mit Angular und Ionic", "Backends mit PHP/Laravel und Datenbanken mit mySQL und PostgreSQL", "User Interface Entwicklung, Audio- und Videotechnik, Computergrafik, Spieleprogrammierung und 3D-Animation"]))
-
     this.currentVitaEntry = this.vitaArray[0];
     this.showVitaEntry(4);
   }
   showMore() {
     if (this.showMoreBtnActive === false) {
       document.getElementById("showMoreBtn").innerText = "Weniger Anzeigen"
-
       this.showMoreBtnActive = true;
-      console.log(this.objArrayShowMore)
       this.objArrayShowMore.forEach(element => {
         this.objArray.push(element);
       });
 
     } else {
-      document.getElementById("showMoreBtn").innerText = "Mehr Anzeigen"  
-      
+      document.getElementById("showMoreBtn").innerText = "Mehr Anzeigen";  
       for (let index = this.objArray.length; index > 6; index--) {
-        console.log(index);  
         this.objArray.pop();  
       }
       this.showMoreBtnActive = false;
